d6c6cf4c1343a4a09a7a5884d8064e1734c47936
Jim Martens <jim1@live.de>
2011-11-23 18:06:18 +0100

    Created the pip, acptemplate and the xml files.

---
+++ src/acptemplates/packageInstallationChangelog.tpl
@@ -0,0 +1,40 @@
+{include file='setupWindowHeader'}
+
+<form method="post" action="index.php?page=Package">
+    <fieldset>
+        <legend>{lang}wcf.acp.package.changelog{/lang}</legend>
+        <div class="inner">
+            <p>{lang}wcf.acp.package.changelog.description{/lang}</p>
+            
+            <div>
+               <p>{@$content}</p>
+            </div>
+            
+            <input type="hidden" name="queueID" value="{@$queueID}" />
+            <input type="hidden" name="action" value="{@$action}" />
+            {@SID_INPUT_TAG}
+            <input type="hidden" name="step" value="{@$step}" />
+            <input type="hidden" name="packageID" value="{@PACKAGE_ID}" />
+            <input type="hidden" name="send" value="send" />
+        </div>
+    </fieldset>
+    
+    
+    <div class="nextButton">
+        <input type="submit" value="{lang}wcf.global.button.next{/lang}" onclick="parent.stopAnimating();" />
+    </div>
+</form>
+
+<script type="text/javascript">
+    //<![CDATA[
+    window.onload = function() {
+        changeHeight();
+    };
+    parent.showWindow(true);
+    parent.setCurrentStep('{lang}wcf.acp.package.step.title{/lang}{lang}wcf.acp.package.step.{if $action == 'rollback'}uninstall{else}{@$action}{/if}.{@$step}{/lang}');
+    
+   
+    //]]>
+</script>
+
+{include file='setupWindowFooter'}
---
+++ src/packageinstallationplugins/ChangelogPackageInstallationPlugin.class.php
@@ -0,0 +1,65 @@
+<?php
+//wcf imports
+require_once(WCF_DIR.'lib/acp/package/plugin/AbstractPackageInstallationPlugin.class.php');
+
+/**
+ * This PIP shows a changelog file.
+ *
+ * @author Jim Martens
+ * @copyright 2011 Jim Martens
+ * @license http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
+ * @package de.plugins-zum-selberbauen.changelog
+ * @subpackage acp.package.plugin
+ * @category Community Framework
+ */
+class ChangelogPackageInstallationPlugin extends AbstractPackageInstallationPlugin {
+    public $tagName = 'changelog';
+    
+    /**
+     * Shows the changelog.
+     */
+    public function install() {
+        parent::install();
+        
+        // get installation path of package
+		$sql = "SELECT	packageDir
+			FROM	wcf".WCF_N."_package
+			WHERE	packageID = ".$this->installation->getPackageID();
+		$packageDir = WCF::getDB()->getFirstRow($sql);
+		$packageDir = $packageDir['packageDir'];
+		
+		//get path of changelog file
+		$changelogTag = $this->installation->getXMLTag($this->tagName);
+		$path = FileUtil::getRealPath(WCF_DIR.$packageDir);
+		
+		//show the changelog
+		$this->show($path.$changelogTag['cdata']);
+		
+    }
+    
+    /**
+     * Shows the changelog file.
+     *
+     * @param string $filename
+     */
+    protected function show($filename) {
+        $content = StringUtil::trim(file_get_contents($filename));
+        WCF::getTPL()->assign('content', $content);
+        WCF::getTPL()->display('packageInstallationChangelog');
+        exit;
+    }
+    
+    /**
+     * Changelog file is part of the files PIP.
+     *
+     * @return boolean false
+     */
+    public function hasUninstall() {
+        return false;
+    }
+    
+    /**
+     * Does nothing.
+     */
+    public function uninstall() {}
+}
